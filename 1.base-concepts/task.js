291539733c3056303a817b729460f5ba4c91e020
adenasky <adenasky@gmail.com>
2024-02-20 20:37:41 +0300

    add task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,43 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	const discriminant = b ** 2 - 4 * a * c;
+
+	let arr = [];
+
+	if (discriminant < 0) {
+		arr = [];
+	} else if (discriminant === 0) {
+		const arr = -b / (2 * a);
+		arr.push(root);
+	} else {
+		const arr1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+		const arr2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+		arr.push(arr1, arr2);
+	}
+
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  percent = parseFloat(percent);
+  contribution = parseFloat(contribution);
+  amount = parseFloat(amount);
+  countMonths = parseInt(countMonths);
+
+  if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths) || percent < 0 || contribution < 0 || amount <= 0 || countMonths <= 0) {
+      return false;
+  }
+
+  let monthlyPercent = percent / 100 / 12;
+
+  let loanBody = amount - contribution;
+
+  let monthlyPayment = loanBody * (monthlyPercent + monthlyPercent / (Math.pow((1 + monthlyPercent), countMonths) - 1));
+
+  let totalAmount = monthlyPayment * countMonths;
+
+  totalAmount = Math.round(totalAmount * 100) / 100;
+
+  return totalAmount;
 }
